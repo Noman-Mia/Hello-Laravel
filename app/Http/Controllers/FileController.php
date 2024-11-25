2afe079c5750a497ee3a3c5d10ee2e523542e214
Noman Mia <nomanislamnishad@gmail.com>
2024-11-25 20:59:44 +0600

    added route and controller

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,4 +9,11 @@ class FileController extends Controller
    function download(){
     return response('File Downloading');
    }
+   
+   function downloadInvoice($invoiceId){
+    return response("Downloading Invoice with Id:{$invoiceId}");
+   }
+   function error(){
+    return response("sorry!no invoice id provided");
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/about', [AboutController::class,'about']);
 Route::get('/download',[FileController::class,'download']);
+Route::get('download/invoice/{invoiceId}',[FileController::class,'downloadInvoice']);
+Route::get('download/invoice',[FileController::class,'error']);
